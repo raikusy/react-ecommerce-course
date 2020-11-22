a7363f63b87db0039008c5cd53efb0949d1f3e14
Shahariar Touhid <shahariar.touhid@gmail.com>
2020-11-23 01:29:02 +0600

    Added Button Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
+import Button from 'components/Button';
 import React from 'react';
+import { BiCartAlt } from 'react-icons/bi';
 
 const Home = () => {
-  return <div>HOME PAGE</div>;
+  return (
+    <div>
+      <Button className="m-2" size="sm">
+        Button
+      </Button>
+      <Button className="m-2" size="md">
+        Button
+      </Button>
+      <Button className="m-2" size="lg">
+        Button
+      </Button>
+      <Button className="m-2 inline-flex items-center" size="sm">
+        <BiCartAlt />
+        Button
+      </Button>
+      <Button className="px-4 py-4">
+        <BiCartAlt />
+      </Button>
+      <Button className="m-2" size="sm">
+        Button
+      </Button>
+    </div>
+  );
 };
 
 export default Home;
