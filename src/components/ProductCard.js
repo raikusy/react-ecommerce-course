b4f881b460809a3982851220e4911b87cfffc05c
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2020-11-25 10:45:20 +0600

    Use tab width 2

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,13 +3,13 @@ import ProductCardBottom from './Product Card/ProductCardBottom';
 import ProductCardTop from './Product Card/ProductCardTop';
 
 function ProductCard({ details, ...props }) {
-    return (
-        <div className="product_card bg-white text-left rounded overflow-hidden hover:shadow-sm transition duration-200 transform hover:-translate-y-1">
-            <ProductCardTop details={details} />
+  return (
+    <div className="product_card bg-white text-left rounded overflow-hidden hover:shadow-sm transition duration-200 transform hover:-translate-y-1">
+      <ProductCardTop details={details} />
 
-            <ProductCardBottom details={details} />
-        </div>
-    );
+      <ProductCardBottom details={details} />
+    </div>
+  );
 }
 
 export default ProductCard;
