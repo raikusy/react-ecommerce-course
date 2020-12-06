14cea67a2c23103f0ec96cc8cd222cedd53d376e
Md Maruf Ahmed <maruf.ahmed76@yahoo.com>
2020-12-06 23:50:00 +0600

    Add Category Slider

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button } from './Button';
+import Button from '../components/Button';
 
 export default {
   title: 'Example/Button',
@@ -14,23 +14,13 @@ const Template = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  primary: true,
-  label: 'Button',
+  size: 'sm',
+  children: 'Button'
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+export const Outline = Template.bind({});
+Outline.args = {
+  varient:"outline",
+  children: 'Button',
+  className : 'px-3 py-3'
 };
